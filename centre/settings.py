8c86151ba5c7674e4dec0afff7d9bbf774667bbc
espaceons <espaceons@gmail.com>
2025-10-08 10:41:34 +0100

    etat de sortie

--- centre/settings.py
+++ centre/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^e2^4c9v4i_h5ejtr&cu!-3xjyus8-$7#3(fe_bt=35obtfw+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '172.16.172.29',]
+ALLOWED_HOSTS = ['*', '172.16.172.9',]
 
 
 # Application definition
