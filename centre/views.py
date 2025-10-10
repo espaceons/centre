855e39832445f934cf26af630643ff832316e256
espaceons <espaceons@gmail.com>
2025-10-10 14:32:28 +0100

    application fini

--- centre/views.py
+++ centre/views.py
@@ -1,9 +1,27 @@
 
 from django.views.generic import TemplateView
 
+from entreprise.models import Entreprise
+from mission.models import Visite
+from personnel.models import Personnel
+
 # === 0. Accueil ===
 
 
 class AccueilView(TemplateView):
     """Affiche la page d'accueil."""
     template_name = 'accueil.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Récupération des décomptes
+        context['total_personnel'] = Personnel.objects.count()
+        context['total_entreprises'] = Entreprise.objects.count()
+        context['total_missions'] = Visite.objects.count()
+
+        # Optionnel : Récupération des 5 dernières missions pour l'affichage
+        context['dernieres_missions'] = Visite.objects.all().order_by(
+            '-date_depart')[:5]
+
+        return context
--- entreprise/models.py
+++ entreprise/models.py
@@ -7,6 +7,7 @@ class Entreprise(models.Model):
     nom = models.CharField(max_length=100)
     adresse = models.CharField(max_length=255)
     secteur_activite = models.CharField(max_length=100)
+    delegation = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.nom
